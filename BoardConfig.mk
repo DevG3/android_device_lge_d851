1243aa27a52bc48cb0ba13eb16b5ef053116d0de
Jose Adolfo Galdamez <josegalre@pac-rom.com>
2016-07-19 10:36:42 -0600

    twrp: update stuff
     * enable tap_to_wake
     * enable cpu 1
     * enable logcat
     * remove thermal control, it's handled in-kernel now
     * use download mode instead of bootloader
     * use correct brightness path
     * remove l-crypto flag
    
    Change-Id: Iaa9376817f92fed8041873b0205b684797581809

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,10 +96,14 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun%d/file"
-TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.175/leds/lcd-backlight/brightness"
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_SUPERSU := true
+TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_L_CRYPTO := true
+TW_MAX_BRIGHTNESS := 255
+TW_NO_REBOOT_BOOTLOADER := true
 TW_THEME := portrait_hdpi
 
 # Charger
