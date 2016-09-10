b74cf4077defffa9ede8da2afc3a7fa135ea50e2
Hendrik Hagendorn <git@finnq.de>
2016-09-10 18:15:01 +0200

    Use BOARD_GLOBAL_CFLAGS and remove antique cflags
    Kill PROPERTY_PERMS_APPEND, last seen in cm-11.0 ...
    
    Change-Id: Ia18e7a82293215db4a5987dfb24a5d24388ce654

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,14 +52,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
-COMMON_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
-COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND=' \
-    { "persist.data.sensor_name", AID_CAMERA, 0 }, \
-    { "camera.4k2k.enable", AID_MEDIA, 0 }, \
-    { "persist.data.rear.minfps", AID_MEDIA, 0 }, \
-    { "persist.data.front.minfps", AID_MEDIA, 0 }, \
-    '
+BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
